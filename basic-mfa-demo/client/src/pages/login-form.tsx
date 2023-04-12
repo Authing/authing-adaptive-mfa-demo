f3f1c8a9e26d8cbb83adeb26c0a2788718261fc4
熊灵 <xiongling@authing.cn>
2023-04-12 15:49:22 +0800

    fix: 删除注释

--- basic-mfa-demo/client/src/pages/login-form.tsx
+++ basic-mfa-demo/client/src/pages/login-form.tsx
@@ -131,33 +131,4 @@ export default function LoginForm() {
       </div>
     </div>
   );
-  // return <div>
-  //   <div className="left-content">
-  //     <img src="https://i.328888.xyz/2023/03/27/intU2Q.png" alt="Your Image"
-  //          className="left-image"/>
-  //       <div className="text-container">
-  //         <p className="left-text-bold">Log in with Authing</p>
-  //         <p className="left-text-small">Your trusted digital identity</p>
-  //       </div>
-  //   </div>
-  //   <div className="navbar">
-  //   </div>
-  //   <div className="container">
-  //     <h1>Login</h1>
-  //     <form>
-  //       <input type="text" id="username" name="username" placeholder="Authing ID" onChange={event => {
-  //         setLoginInfo({ ...loginInfo, username: event.target.value })
-  //       }} />
-  //       <input type="password" id="password" name="password" placeholder="Password" onChange={event => {
-  //         setLoginInfo({ ...loginInfo, password: event.target.value })
-  //       }}/>
-  //       <input type="submit" value="Login" onClick={(e) => onSubmit(e)}/>
-  //     </form>
-  //   </div>
-  //   <div className="footer"></div>
-  //   <div className="right-bottom-text">
-  //     <p>© 2023 Authing inc.</p>
-  //     <p>Last updated on 4 December 2022</p>
-  //   </div>
-  // </div>
 }
